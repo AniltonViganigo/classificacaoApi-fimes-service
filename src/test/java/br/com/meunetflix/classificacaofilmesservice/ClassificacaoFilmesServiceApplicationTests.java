ddea0599ec9abb7ad880413eef5c35379608221b
Anilton Viganigo Junior <viganigojr@gmail.com>
2020-05-28 21:04:14 -0300

    Inicio do projeto

---
+++ src/test/java/br/com/meunetflix/classificacaofilmesservice/ClassificacaoFilmesServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.meunetflix.classificacaofilmesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import sun.tools.jconsole.JConsole;
+
+@SpringBootTest
+class ClassificacaoFilmesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
